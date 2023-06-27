6aac1101aaffc418c8d211c62101f7ad72de755f
Ivan Martynov <ivanmartful@gmail.com>
2023-06-28 01:10:51 +0300

    minor changes

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -81,6 +81,6 @@ class Api {
     }
 }
 
-const api = new Api('api.domainname.ivan.nomoreparties.sbs'); 
+const api = new Api('http://api.domainname.ivan.nomoreparties.sbs'); 
 
 export default api;
